de57184f60df2ee180e679ee7f829cb29bbabc60
Eldrine Le Prismarine <ref.dreamtowards@gmail.com>
2023-10-14 14:05:18 +0800

    lib: jolt 3.0.1

---
+++ lib/jolt-3.0.1/Samples/Tests/ScaledShapes/ScaledCapsuleShapeTest.h
@@ -0,0 +1,16 @@
+// Jolt Physics Library (https://github.com/jrouwe/JoltPhysics)
+// SPDX-FileCopyrightText: 2021 Jorrit Rouwe
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <Tests/Test.h>
+
+class ScaledCapsuleShapeTest : public Test
+{
+public:
+	JPH_DECLARE_RTTI_VIRTUAL(ScaledCapsuleShapeTest)
+
+	// See: Test
+	virtual void	Initialize() override;
+};
---
+++ lib/jolt-3.0.1/Samples/Tests/Shapes/OffsetCenterOfMassShapeTest.h
@@ -0,0 +1,17 @@
+// Jolt Physics Library (https://github.com/jrouwe/JoltPhysics)
+// SPDX-FileCopyrightText: 2021 Jorrit Rouwe
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <Tests/Test.h>
+
+// Tests the OffsetCenterOfMass shape
+class OffsetCenterOfMassShapeTest : public Test
+{
+public:
+	JPH_DECLARE_RTTI_VIRTUAL(OffsetCenterOfMassShapeTest)
+
+	// See: Test
+	virtual void	Initialize() override;
+};
