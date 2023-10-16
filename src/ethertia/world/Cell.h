c3634632d0024c58f84d9a0175e9169625964b42
Eldrine Le Prismarine <ref.dreamtowards@gmail.com>
2023-10-16 17:51:02 +0800

    refac: del world/material dir, no reason exists yet while the path is pretty long

--- src/ethertia/world/Cell.h
+++ src/ethertia/world/Cell.h
@@ -11,15 +11,17 @@ class Cell
 {
 public:
 
-	/// SDF value, 0 -> surface, +0 positive -> void, -0 negative -> solid.
+	/// SDF value, used for Isosurface Extraction.
+	/// 0 -> surface, +0 positive -> void, -0 negative -> solid.
 	float value = 0;  // -inf means nil cell.
 	// TODO: int8 m_Value; float value() => mValue / (float)INT8;
 
 	Material* mtl = nullptr;
 	// TODO: uint16 MtlId
 
-	glm::vec3 fp	{ Math::Inf };	// cached FeaturePoint, Inf=Invalid.  [0, 1]
-	glm::vec3 norm	{ Math::Inf };	// cached Normal.		Inf=Invalid.  [-1, 1]
+	glm::vec3 fp{ Math::Inf };	// cached FeaturePoint, Inf=Invalid.  [0, 1]
+	glm::vec3 norm;				// cached Normal.		Inf=Invalid.  [-1, 1]  if Fp invalid then Norm also means invalid.
+
 
 	bool IsSolid() const { return value > 0; }
 
